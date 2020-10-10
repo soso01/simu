ddcbe07b581c76eedb0072e4a1a8fac68d56922c
soso01 <6125024@gmail.com>
2020-10-10 11:12:35 +0000

    console

--- pages/create.js
+++ pages/create.js
@@ -24,7 +24,7 @@ const create = ({ isUpdate, data }) => {
       },
     ],
   })
-
+	console.log(simData);
   const [isLogin, setIsLogin] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
   const [isPreview, setIsPreview] = useState(false)
